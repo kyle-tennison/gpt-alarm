ae71fd6e54d9c2fb33f8c30e5a7403f4e6567e51
kyle-tennison <kyletennison05@gmail.com>
2026-05-25 21:54:59 -0400

    feat: kill llama in kill script

--- kill.sh
+++ kill.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/bash
 DEBUG_BIN="./target/debug/night-vision"
 RELEASE_BIN="./target/release/night-vision"
+LLAMA_SERVER_BIN="/opt/llama.cpp/build/bin/llama-server"
 
 ps ax | grep "$RELEASE_BIN"
 sudo pkill -f "$RELEASE_BIN"
 
 ps ax | grep "$DEBUG_BIN"
 sudo pkill -f "$DEBUG_BIN"
+
+ps ax | grep "$LLAMA_SERVER_BIN"
+sudo pkill -f "$LLAMA_SERVER_BIN"
